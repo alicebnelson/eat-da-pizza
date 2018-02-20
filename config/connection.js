f840a2cd397bd72e451f71c915262351d1d1329e
alicebnelson <nelsonaliceb@gmail.com>
2018-02-20 12:12:33 -0600

    jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection; 
 
-var connection = mysql.createConnection({
-  port: 8889,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "pizza_db"
-});
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    port: 8889,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "pizza_db"
+  });
+
+};
+
+
 
 // Make connection.
 connection.connect(function(err) {
